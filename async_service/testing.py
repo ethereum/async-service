31f2b3040673f316a5f6e66852b7a648b17f3f5a
Guilherme Salgado <gsalgado@gmail.com>
2019-12-16 10:37:54 +0000

    Refactor test_service_task_cancellation_dag_order and run it against trio

---
+++ async_service/testing.py
@@ -0,0 +1,59 @@
+from async_service import Service
+
+
+async def _test_service_task_cancellation_dag_order(
+        event_class, sleep_fn, cancelled_class, background_service_fn, wait_events_fn):
+    dag = {
+        0: (1, 2, 3),
+        1: (),
+        2: (4, 5),
+        3: (),
+        4: (6, 7, 8, 9, 10),
+        5: (),
+        6: (),
+        7: (),
+        8: (11,),
+        9: (),
+        10: (),
+        11: (12,),
+        12: (13,),
+        13: (),
+    }
+    cancelled = []
+
+    class ServiceTest(Service):
+        def __init__(self):
+            self._task_events = {task_id: event_class() for task_id in dag.keys()}
+
+        async def _do_task(self, task_id):
+            children = dag[task_id]
+            for child_id in children:
+                self.manager.run_task(self._do_task, child_id)
+
+            # yield for a moment to give these time to start
+            await sleep_fn(0)
+            self._task_events[task_id].set()
+            self.manager.logger.info("Task %d started", task_id)
+            try:
+                await self.manager.wait_finished()
+            except cancelled_class:
+                self.manager.logger.info("Task %d cancelled", task_id)
+                cancelled.append(task_id)
+                raise
+
+        async def run(self):
+            await self._do_task(0)
+
+    service = ServiceTest()
+    async with background_service_fn(service) as manager:
+        await wait_events_fn(service._task_events.values())
+        manager.cancel()
+
+    assert len(cancelled) == len(dag)
+
+    seen = set()
+    for value in cancelled:
+        assert value not in seen
+        children = dag[value]
+        assert seen.issuperset(children)
+        seen.add(value)
--- tests-asyncio/test_task_dag_cancellation_order.py
+++ tests-asyncio/test_task_dag_cancellation_order.py
@@ -2,60 +2,16 @@ import asyncio
 
 import pytest
 
-from async_service import Service, background_asyncio_service
+from async_service import background_asyncio_service
+from async_service.testing import _test_service_task_cancellation_dag_order
+
+
+async def wait_events(events):
+    await asyncio.gather(*(event.wait() for event in events))
 
 
 @pytest.mark.asyncio
 async def test_asyncio_service_task_cancellation_dag_order():
-    dag = {
-        0: (1, 2, 3),
-        1: (),
-        2: (4, 5),
-        3: (),
-        4: (6, 7, 8, 9, 10),
-        5: (),
-        6: (),
-        7: (),
-        8: (11,),
-        9: (),
-        10: (),
-        11: (12,),
-        12: (13,),
-        13: (),
-    }
-    cancelled = []
-
-    class ServiceTest(Service):
-        def __init__(self):
-            self._task_events = {task_id: asyncio.Event() for task_id in dag.keys()}
-
-        async def _do_task(self, task_id):
-            children = dag[task_id]
-            for child_id in children:
-                self.manager.run_task(self._do_task, child_id)
-
-            # yield for a moment to give these time to start
-            await asyncio.sleep(0)
-            self._task_events[task_id].set()
-            try:
-                await self.manager.wait_finished()
-            except asyncio.CancelledError:
-                cancelled.append(task_id)
-                raise
-
-        async def run(self):
-            await self._do_task(0)
-
-    service = ServiceTest()
-    async with background_asyncio_service(service) as manager:
-        await asyncio.gather(*(event.wait() for event in service._task_events.values()))
-        manager.cancel()
-
-    assert len(cancelled) == len(dag)
-
-    seen = set()
-    for value in cancelled:
-        assert value not in seen
-        children = dag[value]
-        assert seen.issuperset(children)
-        seen.add(value)
+    await _test_service_task_cancellation_dag_order(
+        asyncio.Event, asyncio.sleep, asyncio.CancelledError, background_asyncio_service,
+        wait_events)
